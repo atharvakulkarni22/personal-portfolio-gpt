6d4bbca318c77916f833bb55f07f35b01aeed4d1
atharvakulkarni22 <atharva.kulkarni99@gmail.com>
2025-06-02 18:19:01 -0500

    Backend api working

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFacePipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 from langchain.chains import RetrievalQA
 from langchain_community.llms import HuggingFaceHub
 from langchain.text_splitter import CharacterTextSplitter
@@ -27,17 +29,26 @@ def get_qa_chain():
     with open("resume_and_cover_letter.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
-    chunks = CharacterTextSplitter(chunk_size=10, chunk_overlap=6).split_text(text)
+    chunks = CharacterTextSplitter(chunk_size=100, chunk_overlap=10).split_text(text)
     docs = [Document(page_content=chunk) for chunk in chunks]
 
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectorstore = FAISS.from_documents(docs, embeddings)
     retriever = vectorstore.as_retriever(search_type="similarity", k=3)
 
-    llm = HuggingFaceHub(
-        repo_id="google/flan-t5-base",
-        model_kwargs={"temperature": 0.5, "max_new_tokens": 256}
+    model_id = "google/flan-t5-base"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "text2text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=256,
+        temperature=0.5
     )
+
+    llm = HuggingFacePipeline(pipeline=pipe)
 
     return RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 
@@ -47,6 +58,7 @@ qa_chain = get_qa_chain()
 def ask_question(query: Query):
     try:
         response = qa_chain.invoke({"query": query.question})
+        print(f"Response: {response}")
         return {"answer": response}
     except Exception as e:
         return {"error": str(e)}
